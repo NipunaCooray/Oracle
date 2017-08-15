4787287e9c5865eb972138311becf0e6b72d04f6
NIpuna Cooray <nipuna.cooray@gmail.com>
2017-08-15 08:30:43 +0530

    Changed add_defects_rpi and add_downtimes_rpi both into POST

--- add_defects_rpi.php
+++ add_defects_rpi.php
@@ -1,35 +1,58 @@
 <?php
-   	include("connect.php");
-   	
-   	$link=Connection();
 
-	$machineNo=$_GET[machineNo];
-	$dt=$_GET[dt];
-   $status=$_GET[status];
-   $a1=$_GET[a1];
-   $a2=$_GET[a2];
-   $a3=$_GET[a3];
-   $b1=$_GET[b1];
-   $b2=$_GET[b2];
-   $c1=$_GET[c1];
-   $c2=$_GET[c2];
-   $d=$_GET[d];
-   $dj=$_GET[dj];
-   $f=$_GET[f];
-   $f1=$_GET[f1];
-   $g1=$_GET[g1];
-   $g2=$_GET[g2];
-   $g3=$_GET[g3];
-   $g4=$_GET[g4];
-   $h1=$_GET[h1];
-   $h2=$_GET[h2];
-   $total=$_GET[total];
 
-	$query = "INSERT INTO `defects` (`machineNo`, `dt`,`status`, `a1`,`a2`, `a3` , `b1`,`b2`, `c1`,`c2`, `d`,`dj`, `f`,`f1`, `g1`,`g2`, `g3`,`g4`, `h1`,`h2`, `total`) 
-		VALUES ('".$machineNo."','".$dt."','".$status."','".$a1."','".$a2."','".$a3."','".$b1."','".$b2."','".$c1."','".$c2."','".$d."','".$dj."','".$f."','".$f1."','".$g1."','".$g2."','".$g3."','".$g4."','".$h1."','".$h2."','".$total."')"; 
-   	
-   	mysql_query($query,$link);
-	   mysql_close($link);
+include("connect.php");
 
-   	header("Location: index.php");
+//POST request
+      
+$link=Connection();
+
+$machineNo = isset($_POST['machineNo']) ? $_POST['machineNo'] : null;
+$dt = isset($_POST['dt']) ? $_POST['dt'] : null;
+$status = isset($_POST['status']) ? $_POST['status'] : null;
+$A = isset($_POST['A']) ? $_POST['A'] : null;
+$A1 = isset($_POST['A1']) ? $_POST['A1'] : null;
+$A2 = isset($_POST['A2']) ? $_POST['A2'] : null;
+$B = isset($_POST['B']) ? $_POST['B'] : null;
+$B1 = isset($_POST['B1']) ? $_POST['B1'] : null;
+$B2 = isset($_POST['B2']) ? $_POST['B2'] : null;
+$B3 = isset($_POST['B3']) ? $_POST['B3'] : null;
+$B4 = isset($_POST['B4']) ? $_POST['B4'] : null;
+$B5 = isset($_POST['B5']) ? $_POST['B5'] : null;
+$B6 = isset($_POST['B6']) ? $_POST['B6'] : null;
+$C = isset($_POST['C']) ? $_POST['C'] : null;
+$C1 = isset($_POST['C1']) ? $_POST['C1'] : null;
+$C2 = isset($_POST['C2']) ? $_POST['C2'] : null;
+$E1 = isset($_POST['E1']) ? $_POST['E1'] : null;
+$E2 = isset($_POST['E2']) ? $_POST['E2'] : null;
+$H = isset($_POST['H']) ? $_POST['H'] : null;
+$H1 = isset($_POST['H1']) ? $_POST['H1'] : null;
+$H2 = isset($_POST['H2']) ? $_POST['H2'] : null;
+$H3 = isset($_POST['H3']) ? $_POST['H3'] : null;
+$H4 = isset($_POST['H4']) ? $_POST['H4'] : null;
+$S1 = isset($_POST['S1']) ? $_POST['S1'] : null;
+$S2 = isset($_POST['S2']) ? $_POST['S2'] : null;
+$S3 = isset($_POST['S3']) ? $_POST['S3'] : null;
+$S4 = isset($_POST['S4']) ? $_POST['S4'] : null;
+$T = isset($_POST['T']) ? $_POST['T'] : null;
+$TT = isset($_POST['TT']) ? $_POST['TT'] : null;
+$V1 = isset($_POST['V1']) ? $_POST['V1'] : null;
+$V2 = isset($_POST['V2']) ? $_POST['V2'] : null;
+$V3 = isset($_POST['V3']) ? $_POST['V3'] : null;
+$V4 = isset($_POST['V4']) ? $_POST['V4'] : null;
+$total = isset($_POST['total']) ? $_POST['total'] : null;
+
+
+ $query = "INSERT INTO `defects` (`machineNo`, `dt`,`status` , `A`,`A1`, `A2`,`B`, `B1`,`B2`, `B3`,`B4`,`B5`,`B6`,`C`,`C1`,`C2`,`E1`,`E2`,`H`,`H1`,`H2`,`H3`,`H4`,`S1`,`S2`,`S3`,`S4`,`T`,`TT`,`V1`,`V2`,`V3`,`V4`,`total`) 
+         VALUES ('".$machineNo."','".$dt."','".$status."','".$A."','".$A1."','".$A2."','".$B."','".$B1."','".$B2."','".$B3."','".$B4."','".$B5."','".$B6."','".$C."','".$C1."','".$C2."','".$E1."','".$E2."','".$H."','".$H1."','".$H2."','".$H3."','".$H4."','".$S1."','".$S2."','".$S3."','".$S4."','".$T."','".$TT."','".$V1."','".$V2."','".$V3."','".$V4."','".$total."')"; 
+         
+$result = mysql_query($query,$link) or die(mysql_error());
+mysql_close($link);
+
+if ($result==1){
+   echo "Successfully saved";
+}else{
+   echo $result;
+}
+
 ?>
--- add_downtimes_rpi.php
+++ add_downtimes_rpi.php
@@ -1,22 +1,28 @@
 <?php
-   	include("connect.php");
+include("connect.php");
+
+$link=Connection();
+
+$downtimetypeid = isset($_POST['downtimetypeid']) ? $_POST['downtimetypeid'] : null;
+$machineno = isset($_POST['machineno']) ? $_POST['machineno'] : null;
+$stoptime = isset($_POST['stoptime']) ? $_POST['stoptime'] : null;
+$starttime = isset($_POST['starttime']) ? $_POST['starttime'] : null;
+$reason = isset($_POST['reason']) ? $_POST['reason'] : null;
+
+
+
+$query = "INSERT INTO `downtimes` (`downtimetypeid`, `machineno`,`stoptime`, `starttime`,`reason`) 
+	VALUES ('".$downtimetypeid."','".$machineno."','".$stoptime."','".$starttime."','".$reason."')"; 
+	
+$result = mysql_query($query,$link) or die(mysql_error());
+mysql_close($link);
+
+
+if ($result==1){
+   echo "Successfully saved";
+}else{
+   echo $result;
+}
+
    	
-   	$link=Connection();
-
-	$downtimetypeid=$_GET[downtimetypeid];
-	$machineno=$_GET[machineno];
-   $stoptime=$_GET[stoptime];
-   $a1=$_GET[a1];
-   $a2=$_GET[a2];
-   $a3=$_GET[a3];
-   $b1=$_GET[b1];
-  
-
-	$query = "INSERT INTO `defects` (`machineNo`, `dt`,`status`, `a1`,`a2`, `a3` , `b1`,`b2`, `c1`,`c2`, `d`,`dj`, `f`,`f1`, `g1`,`g2`, `g3`,`g4`, `h1`,`h2`, `total`) 
-		VALUES ('".$machineNo."','".$dt."','".$status."','".$a1."','".$a2."','".$a3."','".$b1."','".$b2."','".$c1."','".$c2."','".$d."','".$dj."','".$f."','".$f1."','".$g1."','".$g2."','".$g3."','".$g4."','".$h1."','".$h2."','".$total."')"; 
-   	
-   	mysql_query($query,$link);
-	   mysql_close($link);
-
-   	header("Location: index.php");
 ?>
