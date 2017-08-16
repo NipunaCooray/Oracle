6bec62971fdf829e29ba60ba72614eb6a7239a3e
NIpuna Cooray <nipuna.cooray@gmail.com>
2017-08-16 09:17:41 +0530

    Changed index and nav

--- nav.php
+++ nav.php
@@ -83,7 +83,7 @@
     </li>
 
     
-    <li class="content"> <span>Monthly Bandwidth Transfer</span>
+    <!-- <li class="content"> <span>Monthly Bandwidth Transfer</span>
       <div class="progress progress-mini progress-danger active progress-striped">
         <div style="width: 77%;" class="bar"></div>
       </div>
@@ -98,7 +98,7 @@
       </div>
       <span class="percent">87%</span>
       <div class="stat">604.44 / 4000 MB</div>
-    </li>
+    </li> -->
 
   </ul>
 </div>
