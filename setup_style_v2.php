120d4f86e500fba191329259d92482dabb55bd65
Nipuna Cooray <nipuna.cooray@gmail.com>
2017-11-20 14:29:23 +0530

    Changed styledata table

--- setup_style_v2.php
+++ setup_style_v2.php
@@ -1,7 +1,21 @@
 <?php
 
+include("connect.php");
+
+$link=Connection();
+
+$styleNumber = isset($_POST['styleNumber']) ? $_POST['styleNumber'] : null;
+
 $areaList = isset($_POST['areaList']) ? $_POST['areaList'] : null;
 
+$target_dir = "StyleImages/";
+$image_location = $target_dir . basename($_FILES["myFileSelect"]["name"]);
+
+
 echo $areaList;
+
+//print_r (explode(",",$areaList));
+
+mysql_close($link);
 
 ?>
